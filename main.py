afdcba4e16ea6931822b4ed6d5aff465ab29d6e0
Parker <crookparker99@gmail.com>
2025-06-10 10:57:24 -0400

    refactor code by making classes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle
-import pandas
+from score_manager import ScoreManager
+from writer import Writer
+from csv_helper import CsvHelper
 
 IMAGE = "blank_states_img.gif"
 
@@ -7,33 +9,26 @@ screen = turtle.Screen()
 screen.title("U.S States Game")
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
-score = 0
-writer = turtle.Turtle()
-writer.hideturtle()
-writer.penup()
-correct_guesses = []
+score_manager = ScoreManager()
+writer = Writer()
+csv_helper = CsvHelper()
 game_is_on = True
 
 while game_is_on:
 
     # Convert guess to title case
-    answer = turtle.textinput(title=f"{score}/50 States Correct", prompt="What's another state name?").title()
+    answer = turtle.textinput(title=f"{score_manager.score}/50 States Correct", prompt="What's another state name?").title()
 
     # Check if the guess is among the 50 states
-    data = pandas.read_csv("50_states.csv")
-    if data.state.isin([answer]).any():
+    if csv_helper.state_exists(answer):
         # Check if correct guess is already guessed
-        if answer not in correct_guesses:
+        if answer not in score_manager.correct_guesses:
             # Write correct guesses on to the map
-            score += 1
-            x = data[data["state"] == answer]["x"].item()
-            y = data[data["state"] == answer]["y"].item()
-            writer.goto(x, y)
-            writer.write(arg=answer, align="center")
-            correct_guesses.append(answer)
+            score_manager.add_point(answer)
+            writer.write_answer(answer)
 
     # Check game end
-    if len(correct_guesses) == 50:
+    if score_manager.game_won():
         game_is_on = False
 
 screen.exitonclick()
